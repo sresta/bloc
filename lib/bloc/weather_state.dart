b4cfea906ada2430c9bacbf15076094f08e6d99b
unknown <sonika@bajratechnologies.com>
2019-08-15 18:06:44 +0545

    initialise bloc

---
+++ lib/bloc/weather_state.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:bloc_weather/model/weather.dart';
+
+@immutable
+abstract class WeatherState extends Equatable {
+  WeatherState([List props = const <dynamic>[]]) : super(props);
+}
+
+class WeatherInitial extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherLoaded extends WeatherState {
+  final Weather weather;
+  WeatherLoaded(this.weather) : super([weather]);
+}
